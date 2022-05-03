54b93eb94fb9a96dd2f7134ab513281635d1fdf6
Christian Orengia <orengiachristian@gmail.com>
2022-05-03 20:06:07 -0300

    services section improved, new images added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpa } from '@fortawesome/free-solid-svg-icons';
 
 const Service = (props) => {
 
     const { title, img, description } = props
-    const descriptionStyle = { fontSize : '0.9em' }
+    const styleDescription = { fontSize : '1em' }
 
+    const styleParalax = {
+      backgroundImage: 'url("' + img + '")',
+      minHeight: '400px',
+      backgroundAttachment: 'fixed',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }
+/*
   return (
     <article className="col-12 col-md-6 col-lg-4 col-xl-3 p-0 m-0">
         <div className="row py-4 px-2 rounded rounded-3 bg-dark text-light m-1">
@@ -14,6 +25,21 @@ const Service = (props) => {
             <div className="col-12" style={ descriptionStyle } >{ description }</div>
 
         </div>
+    </article>
+  )
+  */
+
+  return (
+    <article className="container-fluid m-0 p-0">
+      <div className="row w-100 p-4 m-0 border-bottom border-light border-3 bg-dark text-light">
+        <div className="col-12 w-100 p-4 m-0" style={ styleDescription }>
+          <h4 className="pb-4 text-uppercase fw-light"><FontAwesomeIcon className="h5 align-middle" icon={ faSpa } /> { title }</h4>
+          <span className="opacity-75">{ description }</span>
+        </div>
+      </div>
+      <div className="row w-100 p-0 m-0">
+        <div className="col-12 w-100 p-0 m-0" style={ styleParalax } />
+      </div>
     </article>
   )
 }
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -3,18 +3,27 @@ import Service from '../Service/Service'
 
 const Services = () => {
   return (
-    <section className="container py-4">
-      <div className="row">
-        
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-        <Service title="Seminario de masajes" img="assets/service1.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante." />
-
-      </div>
+    <section className="container-fluid m-0 p-0">        
+        <Service
+          title="Seminarios de masajes"
+          img="assets/service1.jpg"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante."
+        />
+        <Service
+          title="Cursos de masajes descontracturantes"
+          img="assets/service2.jpg"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante."
+        />
+        <Service
+          title="Seminarios de masoterapia"
+          img="assets/service3.jpg"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante."
+        />
+        <Service
+          title="Cursos web"
+          img="assets/service4.jpg"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat purus lobortis rutrum blandit. Vivamus pharetra erat id risus viverra sodales a nec ante."
+        />
     </section>
   )
 }
